719e7bfe484eb653a7beb614a144ed8cc49c9e25
odedigo <oded.cnaan@gmail.com>
2024-03-20 09:26:32 +0200

    b5

--- assets/js/treasure.js
+++ assets/js/treasure.js
@@ -15,7 +15,7 @@ let rindex = ""
 let debugMode = false
 let deltaAngle = 5;
 let deltaSize = 5;
-let vectorJson = "vectors_rasha.json"
+let vectorJson = "vectors.json"
 
 /**
  * Prints data only if in debug mode
